b697cb400b6de829305cf8fa336f0fa6b62a6f0f
Fabio Carvalho <farvalho@outlook.com>
2022-09-15 13:43:56 +0100

    +Add: Default project

---
+++ Dog/Dog/DogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogApp.swift
+//  Dog
+//
+//  Created by Fábio Carvalho on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
